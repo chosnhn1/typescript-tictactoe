82e30cd60d179e1eb0e1b5a031d67c61f5fa7498
Cho Sunghan <chosnhn1@gmail.com>
2022-08-17 11:01:26 +0900

    add winner

--- ttt/src/components/Board.tsx
+++ ttt/src/components/Board.tsx
@@ -16,6 +16,36 @@ export default function Board() {
     setSquares([...newSqaures])
   }
 
+  const calcWinner = (squares: Array<string>) => {
+    const lines: Array<Array<number>> = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c]: Array<number> = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+
+    return null
+  }
+
+  const winner = calcWinner(squares)
+  let gameStatus: string
+  if (winner) {
+    gameStatus = "Winner: " + winner
+  } else {
+    gameStatus = "Next Player: " + (isNextX? "X" : "O")
+  }
+
   const renderSquare = (i: number) => {
     return (<Square position={i} onClick={(e: React.MouseEventHandler) => handleClick(e, i)} value={squares[i]}>
 
@@ -28,23 +58,23 @@ export default function Board() {
         Board {squares}
       </p>
       <p>
-        Next Player: {isNextX ? "X" : "O"}
+        {gameStatus}
       </p>
       <div className='board'>
         <div className='board-row'>
+          {renderSquare(0)}
           {renderSquare(1)}
           {renderSquare(2)}
-          {renderSquare(3)}
         </div>
         <div className='board-row'>
+          {renderSquare(3)}
           {renderSquare(4)}
           {renderSquare(5)}
-          {renderSquare(6)}
         </div>
         <div className='board-row'>
+          {renderSquare(6)}
           {renderSquare(7)}
           {renderSquare(8)}
-          {renderSquare(9)}
         </div>
       </div>
 

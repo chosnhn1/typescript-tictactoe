e419af90bc02031392b7965e71e7d1c0074b6ec1
Cho Sunghan <chosnhn1@gmail.com>
2022-08-17 19:40:04 +0900

    level up to game

--- ttt/src/components/Board.tsx
+++ ttt/src/components/Board.tsx
@@ -1,65 +1,28 @@
-import React, { useState } from 'react'
+import React, { SetStateAction, useState } from 'react'
 import Square from './Square'
 
 interface Props {
   squares: Array<string>
-  setSquares: React.Dispatch<React.SetStateAction<Array<string>>>
+  handleClick: (i: number) => void
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState<string[]>(Array(10).fill(null))
-  const [isNextX, setIsNextX] = useState<boolean>(true)
-  const handleClick = (event: React.MouseEventHandler, i: number) => {
-    const newSqaures: Array<string> = squares.slice()
-    newSqaures[i] = isNextX ? "X" : "O"
-    setIsNextX(isNextX ? false : true )
-    setSquares([...newSqaures])
-  }
-
-  const calcWinner = (squares: Array<string>) => {
-    const lines: Array<Array<number>> = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ]
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c]: Array<number> = lines[i]
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a]
-      }
-    }
-
-    return null
-  }
-
-  const winner = calcWinner(squares)
-  let gameStatus: string
-  if (winner) {
-    gameStatus = "Winner: " + winner
-  } else {
-    gameStatus = "Next Player: " + (isNextX? "X" : "O")
-  }
-
+export default function Board(props: Props) {
+  
   const renderSquare = (i: number) => {
-    return (<Square position={i} onClick={(e: React.MouseEventHandler) => handleClick(e, i)} value={squares[i]}>
-
+    return (<Square
+      position={i}
+      value={props.squares[i]}
+      onClick={() => props.handleClick(i)}>
     </Square>)
   }
+
 
   return (
     <div className='game'>
       <p>
-        Board {squares}
+        Board {props.squares}
       </p>
-      <p>
-        {gameStatus}
-      </p>
+      
       <div className='board'>
         <div className='board-row'>
           {renderSquare(0)}
--- ttt/src/pages/Game.tsx
+++ ttt/src/pages/Game.tsx
@@ -2,12 +2,79 @@ import React, { useState } from 'react'
 import Board from '../components/Board'
 
 export default function Game() {
-  const [player, setPlayer] = useState("X")
+  type SquareType = Array<string> | null
+  type HistoryType = {
+    squares: Array<string|null>
+  }
+  const [history, setHistory] = useState<Array<HistoryType>>(
+    [{squares: Array(9).fill(null)}]
+  )
+  const [isNextX, setIsNextX] = useState<boolean>(true)
+  
+  const current: unknown = history[history.length - 1]
+  
+
+
+
+
+
+
+
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(null))
+
+  const calcWinner = (squares: Array<string>) => {
+    const lines: Array<Array<number>> = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c]: Array<number> = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+
+    return null
+  }
+
+  const winner = calcWinner(squares)
+  let gameStatus: string
+  if (winner) {
+    gameStatus = "Winner: " + winner
+  } else {
+    gameStatus = "Next Player: " + (isNextX? "X" : "O")
+  }
+
+  const handleClick = (i: number) => {
+    const newSquares: Array<string> = squares.slice()
+    if (calcWinner(squares) || squares[i]) {
+      return
+    }
+
+    newSquares[i] = isNextX ? "X" : "O"
+    setIsNextX(!isNextX)
+    setSquares([...newSquares])
+  }
+
 
   return (
-    <div>
+    <div className="game">
       <p>Game</p>
-      <Board />
+      <div className="game-board">
+        <Board squares={squares} handleClick={handleClick} />
+      </div>
+      <div className="game-info">
+        {gameStatus}
+      </div>
+
+
     </div>
   )
 }
